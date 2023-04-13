f1a6f06435c37047acf872ea81d5312c37789475
chubbi stephen <udeokoliestephen@gmail.com>
2023-04-13 19:12:00 +0100

    Working on active state for the nav elements.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,46 +1,23 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { useState } from "react"
 
 function Navbar() {
-  const [isActive, setIsActive] = useState(false)
-
-  // const handleClick = (e) => {
-  //   setIsActive((current) => !current)
-  // }
-
-   const handleClick = (event) => {
-     event.currentTarget.classList.toggle("active")
-   }
-
   return (
     <nav className="flex">
       <ul className="w-[100%] flex text-lg font-medium my-8 space-x-20 ">
         <h1 className="text-[2rem] specialText font-bold">Juddie.</h1>
         <li>
-          <Link
-            to="/"
-            // className={isActive ? "active" : ""}
-            onClick={handleClick}
-          >
+          <Link to="/" className="text-black active:text-cyan-400">
             Home
           </Link>
         </li>
         <li>
-          <Link
-            to="/about"
-            // className={isActive ? "active" : " "}
-            onClick={handleClick}
-          >
+          <Link to="/about" className="text-black active:text-cyan-400">
             About
           </Link>
         </li>
         <li>
-          <Link
-            to="/contact"
-            // className={isActive ? "active" : ""}
-            onClick={handleClick}
-          >
+          <Link to="/contact" className="text-black active:text-cyan-400">
             Contact us
           </Link>
         </li>
@@ -48,7 +25,10 @@ function Navbar() {
           <Link>Cart</Link>
         </li>
         <li>
-          <Link to='/login'> Login</Link>
+          <Link to="/login" className="text-black active:text-cyan-400">
+            {" "}
+            Login
+          </Link>
         </li>
       </ul>
     </nav>
