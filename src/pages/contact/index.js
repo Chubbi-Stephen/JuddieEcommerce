21b353c855eeea779ac435670b8dca54260fa5d3
chubbi stephen <udeokoliestephen@gmail.com>
2023-04-15 07:14:35 +0100

    completed the task at hand

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -189,7 +189,7 @@ function Contact() {
             <div className="mt-10">
               <button
                 type="submit"
-                className="block w-full rounded-md box px-3.5 py-2.5 text-center text-sm font-semibold text-black shadow-sm ndigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="block w-full rounded-md box px-3.5 py-2.5 text-center text-sm font-semibold text-black shadow-sm ndigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 talk"
               >
                 Let's talk
               </button>
